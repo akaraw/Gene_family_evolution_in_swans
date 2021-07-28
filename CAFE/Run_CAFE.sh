43c81be27b94ee413276a09ed7355f53fa3b3893
Anjana_CK <43054305+akaraw@users.noreply.github.com>
2021-07-28 21:42:50 +1000

    Update Run_CAFE.sh

--- CAFE/Run_CAFE.sh
+++ CAFE/Run_CAFE.sh
@@ -2,12 +2,12 @@
 #timetree.org
 
 #cat SpeciesTree_rooted.txt.ultrametric.tre
-''
-(((Oryzias_latipes:128.00000000,Gasterosteus_aculeatus:128.00000000)'14':101.90276097,Danio_rerio:229.90276097)'13':205.42131365,(Xenopus_tropicalis:351.75848322,((Anolis_carolinensis:279.65697667,((Coturnix_japonica:42.00000000,Gallus_gallus:42.00000000)'11':37.95541635,((Cygnus_olor:6.08027000,Cygnus_atratus:6.08027000)'10':24.24849644,Anas_platyrhynchos:30.32876644)'19':49.62664992)'9':199.70156031)'22':32.24694470,((Monodelphis_domestica:158.59758758,((Canis_lupus:77.75495641,Bos_taurus:77.75495641)'8':18.70743276,((Rattus_norvegicus:20.88741740,Mus_musculus:20.88741740)'6':68.93577002,Homo_sapiens:89.82318742)'30':6.63920175)'29':62.13519841)'27':18.32991064,Ornithorhynchus_anatinus:176.92749821)'35':134.97642315)'43':39.85456185)'42':83.56559140);
-''
+
+(((Oryzias_latipes:128.00000000,Gasterosteus_aculeatus:128.00000000)'14':101.90276097,Danio_rerio:229.90276097)'13':205.42131365,(Xenopus_tropicalis:351.75848322,((Anolis_carolinensis:279.65697667,((Coturnix_japonica:42.00000000,Gallus_gallus:42.00000000)'11':37.95541635,((Cygnus_olor:6.08027000,Cygnus_atratus:6.08027000)'10':24.24849644,Anas_platyrhynchos:30.32876644)'19':49.62664992)'9':199.70156031)'22':32.24694470,((Monodelphis_domestica:158.59758758,(((Felis_catus:54.32144118,Canis_lupus:54.32144118)'8':23.43351523,Bos_taurus:77.75495641)'6':18.70743276,((Rattus_norvegicus:20.88741740,Mus_musculus:20.88741740)'30':68.93577002,Homo_sapiens:89.82318742)'29':6.63920175)'27':62.13519841)'35':18.32991064,Ornithorhynchus_anatinus:176.92749821)'43':134.97642315)'42':39.85456185)'40':83.56559140);
 
 #Create the lambda tree by copying the above tree and replacing the branch length with lambda index e.g: 1,2,3 etc.
-(((Oryzias_latipes:1,Gasterosteus_aculeatus:1):1,Danio_rerio:1):1,(Xenopus_tropicalis:2,((Anolis_carolinensis:3,((Coturnix_japonica:3,Gallus_gallus:3):3,((Cygnus_olor:3,Cygnus_atratus:3):3,Anas_platyrhynchos:3):3):3):3,((Monodelphis_domestica:4,((Canis_lupus:4,Bos_taurus:4):4,((Rattus_norvegicus:4,Mus_musculus:4):4,Homo_sapiens:4):4):4):4,Ornithorhynchus_anatinus:4):2):2):1);
+(((Oryzias_latipes:1,Gasterosteus_aculeatus:1):1,Danio_rerio:1):1,(Xenopus_tropicalis:2,((Anolis_carolinensis:3,((Coturnix_japonica:3,Gallus_gallus:3):3,((Cygnus_olor:3,Cygnus_atratus:3):3,Anas_platyrhynchos:3):3):3):3,((Monodelphis_domestica:4,(((Felis_catus:4,Canis_lupus:4):4,Bos_taurus:4):4,((Rattus_norvegicus:4,Mus_musculus:4):4,Homo_sapiens:4):4):4):4,Ornithorhynchus_anatinus:4):2):2):1);
+
 
 awk -F'\t' '{print "(null)\t"$0}' Orthogroups.GeneCount.tsv > tmp.tsv
 
