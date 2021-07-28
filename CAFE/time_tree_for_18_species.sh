e60c7ea4d162dca6a6a4fe990d04a8b038bac9b3
Anjana_CK <43054305+akaraw@users.noreply.github.com>
2021-07-28 13:55:16 +1000

    Create time_tree_for_18_species.sh

---
+++ CAFE/time_tree_for_18_species.sh
@@ -0,0 +1,24 @@
+#List of species
+Danio rerio
+Gasterosteus aculeatus
+Oryzias latipes
+Xenopus tropicalis
+Anolis carolinensis
+Coturnix japonica
+Gallus gallus
+Cygnus olor
+Cygnus atratus
+Anas platyrhynchos
+Ornithorhynchus anatinus
+Monodelphis domestica
+Bos taurus
+Canis familiaris
+Homo sapiens
+Mus musculus
+Rattus norvegicus
+
+#Upload the list to the timetree.org and download the tree in the newick format (Please see the figure)
+
+#Newick tree
+(((Oryzias_latipes:128.00000000,Gasterosteus_aculeatus:128.00000000)'14':101.90276097,Danio_rerio:229.90276097)'13':205.42131365,(Xenopus_tropicalis:351.75848322,((Anolis_carolinensis:279.65697667,((Coturnix_japonica:42.00000000,Gallus_gallus:42.00000000)'11':37.95541635,((Cygnus_olor:6.08027000,Cygnus_atratus:6.08027000)'10':24.24849644,Anas_platyrhynchos:30.32876644)'19':49.62664992)'9':199.70156031)'22':32.24694470,((Monodelphis_domestica:158.59758758,((Canis_lupus:77.75495641,Bos_taurus:77.75495641)'8':18.70743276,((Rattus_norvegicus:20.88741740,Mus_musculus:20.88741740)'6':68.93577002,Homo_sapiens:89.82318742)'30':6.63920175)'29':62.13519841)'27':18.32991064,Ornithorhynchus_anatinus:176.92749821)'35':134.97642315)'43':39.85456185)'42':83.56559140);
+
