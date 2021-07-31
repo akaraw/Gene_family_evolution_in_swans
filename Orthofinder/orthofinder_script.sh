9f6bfe95a7bb07f9d6b4397fe41f4116d9ed7523
Anjana_CK <ackarawita@yahoo.com>
2021-07-31 12:36:08 +1000

    Update orthofinder_script.sh

--- Orthofinder/orthofinder_script.sh
+++ Orthofinder/orthofinder_script.sh
@@ -2,6 +2,7 @@ mamba create -n ortho -c bioconda orthofinder
 
 #The prmiary_transcript folder contains the proteomes dwonloaded from the ENSEMBL
 #The final longest isoform models were incoroporated (for both black swan and mute swan)
+#python2 ~/miniconda3/envs/ortho/bin/primary_transcript.py
 
 orthofinder -t 20 -p ${TMPDIR}/ -o results -f primary_transcripts
 
