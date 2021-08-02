2e55d65b4147b926b5a5682c3dbcd60bdc11962f
Anjana_CK <ackarawita@yahoo.com>
2021-08-02 20:36:11 +1000

    Update hmmerscan.sh

--- TreeFam/hmmerscan.sh
+++ TreeFam/hmmerscan.sh
@@ -4,11 +4,9 @@ for i in *.fa
 do
 base=$(basename $i ".fa")
 hmmscan --tblout ${base}.txt --cpu 20 -E 1e-3 --noali hmm_lib/TreeFam9 $i
-grep -v "#\|^$" ${base}.txt |sed -e 's/  */\t/g'| sort -k3,3n -k5,5g | awk '!($3 in a){a[$3];print}' > ${base}.txt
-./line.sh 1 ${base}.txt > ${base}
+grep -v "#\|^$" ${base}.txt |sed -e 's/  */\t/g'| sort -k3,3n -k5,5g | awk '!($3 in a){a[$3];print}' > tmp.${base}.txt
+./line.sh 1 tmp.${base}.txt > ${base}
 file=${base}
 awk 'NR==1 { print "Desc""\t"FILENAME }; 1' "$file" > temp && mv temp final.${base}.tsv
-rm ${base}
-#rm ${base}.txt
-rm ${base}.tsv tmp.${base}.tsv
+rm ${base} tmp.${base}.txt 
 done 
