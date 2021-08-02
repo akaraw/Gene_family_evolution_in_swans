04c468d3ebf243c339033c3bd84126ee3adf37b5
Anjana_CK <ackarawita@yahoo.com>
2021-08-03 06:06:37 +1000

    Create hmmscan_parallel.sh

---
+++ TreeFam/hmmscan_parallel.sh
@@ -0,0 +1,7 @@
+seqkit split $genome -p 24
+function hmmer() {
+base=$(basename "$1")
+hmmscan --tblout ${base}.txt --cpu 1 -E 1e-5 --noali hmm_lib2/TreeFam9 $1
+}
+export -f hmmer
+find $genome.split/ -type f -name "*fa" | parallel -j 24 hmmer {}
