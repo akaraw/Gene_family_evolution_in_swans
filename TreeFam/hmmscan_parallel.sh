ca4d5b2c840c7a4116e42b9e80380a78ed614ef8
Anjana_CK <ackarawita@yahoo.com>
2021-08-03 10:25:43 +1000

    Update hmmscan_parallel.sh

--- TreeFam/hmmscan_parallel.sh
+++ TreeFam/hmmscan_parallel.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/bash
 genome=$1
 seqkit split $genome -p 24
 function hmmer() {
@@ -6,3 +7,9 @@ hmmscan --tblout ${base}.txt --cpu 1 -E 1e-5 --noali hmm_lib2/TreeFam9 $1
 }
 export -f hmmer
 find $genome.split/ -type f -name "*fa" | parallel -j 24 hmmer {}
+cat ${base}.part_*.fa.txt > ${base}.txt
+grep -v "#\|^$" ${base}.txt |sed -e 's/  */\t/g'| sort -k3,3n -k5,5g | awk '!($3 in a){a[$3];print}' > tmp.${base}.txt
+./line.sh 1 tmp.${base}.txt > ${base}
+file=${base}
+awk 'NR==1 { print "Desc""\t"FILENAME }; 1' "$file" > temp && mv temp final.${base}.tsv
+rm ${base} tmp.${base}.txt 
