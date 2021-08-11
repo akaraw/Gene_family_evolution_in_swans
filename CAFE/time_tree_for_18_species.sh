f7e6d1367de350fd9d1e271e2595caace90cfae2
Anjana_CK <ackarawita@yahoo.com>
2021-08-11 20:55:17 +1000

    Update time_tree_for_18_species.sh

--- CAFE/time_tree_for_18_species.sh
+++ CAFE/time_tree_for_18_species.sh
@@ -12,15 +12,10 @@ Taeniopygia guttata
 Ornithorhynchus anatinus
 Monodelphis domestica
 Bos taurus
-Canis familiaris
 Homo sapiens
 Mus musculus
-Rattus norvegicus
-Felis catus
 Meleagris_gallopavo.fa
-
+Ficedula albicollis
 #Upload the list to the timetree.org and download the tree in the newick format (Please see the figure)
 
 #Newick tree
-(((Oryzias_latipes:128.00000000,Gasterosteus_aculeatus:128.00000000)'14':101.90276097,Danio_rerio:229.90276097)'13':205.42131365,(Xenopus_tropicalis:351.75848322,((Anolis_carolinensis:279.65697667,(((Meleagris_gallopavo:37.20000000,Gallus_gallus:37.20000000)'11':42.75541635,((Cygnus_olor:6.08027000,Cygnus_atratus:6.08027000)'10':24.24849644,Anas_platyrhynchos:30.32876644)'19':49.62664992)'9':18.08745294,Taeniopygia_guttata:98.04286929)'22':181.61410737)'8':32.24694470,((Monodelphis_domestica:158.59758758,(((Felis_catus:54.32144118,Canis_lupus:54.32144118)'6':23.43351523,Bos_taurus:77.75495641)'30':18.70743276,((Rattus_norvegicus:20.88741740,Mus_musculus:20.88741740)'29':68.93577002,Homo_sapiens:89.82318742)'27':6.63920175)'35':62.13519841)'43':18.32991064,Ornithorhynchus_anatinus:176.92749821)'42':134.97642315)'40':39.85456185)'48':83.56559140);
-
